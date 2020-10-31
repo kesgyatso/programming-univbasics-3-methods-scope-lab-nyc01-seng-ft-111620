29ec00299b771a5b4a503a3405940eb10ad2db2e
Kelsang Gyatso <kelsanggyatso@gmail.com>
2020-10-31 15:17:10 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -9,4 +9,12 @@ def toadstool
 end
 
 def link
-  phrase = 
+  phrase =  "It's Dangerous To Go Alone! Take This."
+  puts phrase
+end
+
+def all_phrases
+  puts mario
+  puts toadstool
+  puts link
+end
